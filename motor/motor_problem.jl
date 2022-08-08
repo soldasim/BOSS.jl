f095ed62470478b729c962a334bd16bf0cee39a9
Sheld5 <simonsoldat@seznam.cz>
2022-08-08 17:48:07 +0200

    add LHC init data sampling

--- motor/motor_problem.jl
+++ motor/motor_problem.jl
@@ -1,6 +1,8 @@
 using Random
 using Distributions
 using PyCall
+using LatinHypercubeSampling
+using FLoops
 
 include("../boss.jl")
 include("../example/data.jl")
@@ -29,11 +31,11 @@ domain_ub() = [59.9, 0.029, 0.3999] #[60., 0.03, 0.400]
 # main function
 function example(max_iters; info=true, kwargs...)
     info && print("generating init data ...\n")
-    X, Y, Z = generate_init_data_(19)
+    X, Y, Z = generate_init_data_LHC_(19)
     info && print("generating test data ...\n")
     test_X, test_Y = generate_test_data_(20)
 
-    Z = nothing  # TODO rem
+    Z = nothing  # TODO remove
     res = run_boss_(X, Y, Z; max_iters, info, test_X, test_Y, kwargs...)
     return res, (test_X, test_Y)
 end
@@ -53,26 +55,29 @@ end
 
 function compare_models(; save_run_data=false, filename="rundata.jld2", make_plots=false)
     # generate data
-    X, Y, Z = generate_init_data_(19)
     test_X, test_Y = generate_test_data_(20)
 
     # experiment settings
-    runs = 16
-    max_iters = 20
+    runs = 2#16
+    max_iters = 2#200
+    init_data_size = 19
 
     print("Starting $runs runs.\n")
     results = [Vector{RunResult}(undef, runs) for _ in 1:3]
-    for i in 1:runs  # parallel: Threads.@threads
+    for i in 1:runs  # parallel: @floop
         print("Thread $(Threads.threadid()):  Run $i in progress ...\n")
         info = true
+
+        X, Y, Z = generate_init_data_LHC_(init_data_size)
+        Z = nothing  # TODO remove
 
         param_res = run_boss_(X, Y, Z; use_model=:param, max_iters, info, test_X, test_Y, make_plots)
         semiparam_res = run_boss_(X, Y, Z; use_model=:semiparam, max_iters, info, test_X, test_Y, make_plots)
         nonparam_res = run_boss_(X, Y, Z; use_model=:nonparam, max_iters, info, test_X, test_Y, make_plots)
 
-        results[1][i] = param_res
-        results[2][i] = semiparam_res
-        results[3][i] = nonparam_res
+        @inbounds results[1][i] = param_res
+        @inbounds results[2][i] = semiparam_res
+        @inbounds results[3][i] = nonparam_res
     end
 
     labels = ["param", "semiparam", "nonparam"]
@@ -84,8 +89,7 @@ end
 
 # wrapper function
 function run_boss_(init_X, init_Y, init_Z; kwargs...)
-    sample_count = 20#200
-    util_opt_multistart = 10#100
+    util_opt_multistart = 100
 
     noise_pred = [0.01, 0.01, 0.01]  # TODO modify
     constraint_noise = [0.01, 0.01, 0.01]
@@ -97,7 +101,6 @@ function run_boss_(init_X, init_Y, init_Z; kwargs...)
         f_true=x->fg_true(x)[1],
         noise=noise_pred,
         constraint_noise,
-        sample_count,
         util_opt_multistart,
         target_err=nothing,
         kwargs...
@@ -106,8 +109,19 @@ function run_boss_(init_X, init_Y, init_Z; kwargs...)
     return RunResult(time, X, Y, Z, bsf, errs)
 end
 
-function generate_init_data_(size)
-    X = hcat(rand(Product(Distributions.Uniform.(domain_lb(), domain_ub())), size))'
+function generate_init_data_random_(size)
+    lb, ub = domain_lb(), domain_ub()
+    X = hcat(rand(Product(Distributions.Uniform.(lb, ub)), size))'
+    return generate_init_data_(X, size)
+end
+
+function generate_init_data_LHC_(size)
+    lb, ub = domain_lb(), domain_ub()
+    X = scaleLHC(randomLHC(size, 3), [(lb[i], ub[i]) for i in 1:3])
+    return generate_init_data_(X, size)
+end
+
+function generate_init_data_(X, size)
     Y = zeros(size, 3)
     Z = zeros(size, 1)
     for i in 1:size
