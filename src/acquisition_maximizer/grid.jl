6b7e7a1b27041ce43bc0e48fc6d172df7cdd719f
Sheld5 <simonsoldat@seznam.cz>
2024-03-10 18:08:59 +0100

    add random shuffle to GridAM

--- src/acquisition_maximizer/grid.jl
+++ src/acquisition_maximizer/grid.jl
@@ -1,3 +1,4 @@
+using Random
 
 """
     GridAM(problem, steps; kwargs...)
@@ -20,28 +21,31 @@ struct GridAM <: AcquisitionMaximizer
     points::Vector{Vector{Float64}}
     steps::Vector{Float64}
     parallel::Bool
+    shuffle::Bool
 end
 function GridAM(;
     problem::BOSS.OptimizationProblem,
     steps,
     parallel=true,
+    shuffle=true,
 )
     domain = problem.domain
     ranges = [domain.bounds[1][i]:steps[i]:domain.bounds[2][i] for i in 1:x_dim(problem)]
     points = [[x...] for x in Iterators.product(ranges...) if in_domain([x...], domain)]
-    return GridAM(points, steps, parallel)
+    return GridAM(points, steps, parallel, shuffle)
 end
 
 function maximize_acquisition(acq::Function, opt::GridAM, problem::BOSS.OptimizationProblem, options::BossOptions)
+    points_ = opt.shuffle ? (opt.points |> deepcopy |> shuffle) : opt.points
     if opt.parallel
-        vals = Vector{Float64}(undef, length(opt.points))
-        Threads.@threads for i in eachindex(opt.points)
-            p = opt.points[i]
+        vals = Vector{Float64}(undef, length(points_))
+        Threads.@threads for i in eachindex(points_)
+            p = points_[i]
             v = acq(p)
             vals[i] = v
         end
-        return opt.points[argmax(vals)]
+        return points_[argmax(vals)]
     else
-        return argmax(acq, opt.points)
+        return argmax(acq, points_)
     end
 end
