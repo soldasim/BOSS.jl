70b4c847bfa7b453351232df3cd39f6b0c609f9b
soldasim <simonsoldat@seznam.cz>
2024-11-04 18:50:03 +0100

    up dep versions; change '\' to '/' in test paths

--- test/unit/runtests.jl
+++ test/unit/runtests.jl
@@ -4,13 +4,13 @@ include("_parametrized_tests.jl")  # TODO
 function include_unit_tests(dir)
     for file in readdir(dir)
         if endswith(file, ".jl")
-            @info "  Testing $dir\\$file"
+            @info "  Testing $dir/$file"
             @testset "$file" begin
-                include(dir*'\\'*file)
+                include(dir*'/'*file)
             end
         else
             @testset "$file" begin
-                include_unit_tests(dir*'\\'*file)
+                include_unit_tests(dir*'/'*file)
             end
         end
     end
@@ -18,5 +18,5 @@ end
 
 @testset "Unit Tests" begin
     @info "Running Unit Tests ..."
-    include_unit_tests(Base.source_dir() * "\\test")
+    include_unit_tests(Base.source_dir() * '/' * "test")
 end
