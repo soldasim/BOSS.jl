f0c297debb607d297dcdca24c41144fa80050c0f
soldasim <simonsoldat@seznam.cz>
2025-01-09 11:39:31 +0100

    improve init data docs

--- src/data/data.jl
+++ src/data/data.jl
@@ -14,6 +14,10 @@ Base.isempty(data::ExperimentData) = isempty(data.X)
 """
 Stores the initial data.
 
+At least one initial datapoint has to be provided (purely for implementation reasons).
+One can for example use LatinHypercubeSampling.jl to obtain a small intial grid,
+or provide a single random initial datapoint.
+
 # Fields
 - `X::AbstractMatrix{<:Real}`: Contains the objective function inputs as columns.
 - `Y::AbstractMatrix{<:Real}`: Contains the objective function outputs as columns.
